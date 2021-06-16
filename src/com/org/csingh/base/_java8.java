9e40c55e392f4c5b2e9b198b92a43dcc094d3428
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-16 07:17:57 +0530

    terminal operation vs no terminal operation with example of stream

--- src/com/org/csingh/base/_java8.java
+++ src/com/org/csingh/base/_java8.java
@@ -175,6 +175,22 @@ class _java8 {
 	    assertEquals("john", name);
 	}
 	
+	@Test
+	public void IntermediateandTerminalOperations() {
+		System.out.println("Stream without terminal operation");
+	    
+	    Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
+	        System.out.println("doubling " + i);
+	        return i * 2;
+	    });
+	 
+	    System.out.println("Stream with terminal operation");
+	        Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
+	            System.out.println("doubling " + i);
+	            return i * 2;
+	    }).sum();
+	}
+	
 	
 	
 
