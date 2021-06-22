90bfcd1089ddd13f54f0090e979ead1d1e2d6a84
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-22 10:54:58 +0530

    Collectors in java 8 example

--- src/com/org/csingh/base/_java_8_way_of_doing_things.java
+++ src/com/org/csingh/base/_java_8_way_of_doing_things.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,17 @@ class _java_8_way_of_doing_things {
 		List<Integer> numbers = Arrays.asList(5, 4, 10, 12);
 		IntSummaryStatistics summary = numbers.stream().mapToInt( val -> val).summaryStatistics();
 		assertEquals(31, summary.getSum());
+	}
+	
+	//collectors in Java 8
+	@Test
+	void collectors_in_Java_8() {
+		System.out.println("Collectors used to combine result of all operations");
+		List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+		List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+		System.out.println("Filtered List: " + filtered);
+		String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+		System.out.println("Merged String: " + mergedString);
 	}
 
 }
