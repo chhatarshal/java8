2169eeff0113466bf754725bb0d8572792f7bdce
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-07-26 10:20:11 +0530

    more reduce example

--- src/com/org/csingh/base/_Stream_reduce_example.java
+++ src/com/org/csingh/base/_Stream_reduce_example.java
@@ -31,6 +31,20 @@ class _Stream_reduce_example {
 		  assertEquals(55, sum_java8);
 		  
 		  assertEquals(56, Arrays.stream(numbers_java8).reduce(1, (a, b) -> a + b));
+		  
+		  int sum1 = Arrays.stream(numbers).reduce(0, (a, b) -> a + b);    // 55
+		  int sum2 = Arrays.stream(numbers).reduce(0, Integer::sum);      // 55
+
+		  int sum3 = Arrays.stream(numbers).reduce(0, (a, b) -> a - b);   // -55
+		  int sum4 = Arrays.stream(numbers).reduce(0, (a, b) -> a * b);   // 0, initial is 0, 0 * whatever = 0
+		  int sum5 = Arrays.stream(numbers).reduce(0, (a, b) -> a / b);  
+		  assertEquals(55, sum1);
+		  assertEquals(55, sum2);
+		  
+		  assertEquals(-55, sum3);
+		  assertEquals(0, sum4);
+		  assertEquals(0, sum5);
+		  System.out.println(sum5);
 	}
 
 }
