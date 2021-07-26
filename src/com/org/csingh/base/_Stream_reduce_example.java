2615133281cc06f40df29f7416629fd97fa3dd32
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-07-26 10:31:46 +0530

    map reduce example java8

--- src/com/org/csingh/base/_Stream_reduce_example.java
+++ src/com/org/csingh/base/_Stream_reduce_example.java
@@ -2,14 +2,17 @@ package com.org.csingh.base;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class _Stream_reduce_example {
 
 	@Test
-	void test() {
+	void reduce_example() {
 
 		  int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		  int sum = 0;
@@ -46,5 +49,49 @@ class _Stream_reduce_example {
 		  assertEquals(0, sum5);
 		  System.out.println(sum5);
 	}
+	
+	@Test
+	void map_and_reduce() {
+		List<Invoice> invoices = Arrays.asList(
+                new Invoice("A01", BigDecimal.valueOf(9.99), BigDecimal.valueOf(1)),
+                new Invoice("A02", BigDecimal.valueOf(19.99), BigDecimal.valueOf(1.5)),
+                new Invoice("A03", BigDecimal.valueOf(4.99), BigDecimal.valueOf(2))
+        );
+
+        BigDecimal sum = invoices.stream()
+                .map(x -> x.getQty().multiply(x.getPrice()))    // map
+                .reduce(BigDecimal.ZERO, BigDecimal::add);      // reduce
+System.out.println("test case 2...   " + sum);
+        System.out.println(sum);    // 49.955
+      //  assertEquals(49.955, sum);
+        System.out.println(sum.setScale(2, RoundingMode.HALF_UP));  // 49.96
+       
+
+    }
+	
+	class Invoice {
+
+	    String invoiceNo;
+	    BigDecimal price;
+	    BigDecimal qty;
+	    
+	    public Invoice(String invoiceNo, BigDecimal price, BigDecimal qty) {
+	    	this.invoiceNo = invoiceNo;
+	    	this.price = price;
+	    	this.qty= qty;
+	    }
+
+		public BigDecimal getQty() {
+			return this.qty;
+		}
+
+		public BigDecimal getPrice() {
+			return this.price;
+		}
+
+	    // getters, stters n constructor
+	}
+
+	
 
 }
